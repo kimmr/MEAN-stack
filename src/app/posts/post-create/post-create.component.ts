557eeb4c21650e05e237ff3e987b82bc04458175
kimmr <kimmr@vcu.edu>
2021-02-19 00:03:06 -0500

    Angular setup is added

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+
+export class PostCreateComponent {
+  userPost = '';
+  newPost = '';
+
+  onAddPost() {
+    this.newPost = this.userPost
+  }
+}
